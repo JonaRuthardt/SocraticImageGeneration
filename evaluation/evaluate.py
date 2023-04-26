831d7d28591164f8426dcbffb1e58ebd6ce7a9ba
Jona Ruthardt <jona@ruthardt.de>
2023-04-26 16:52:10 +0200

    Started on first draft of evaluation pipeline

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -1,5 +1,109 @@
 import os, sys
+import json
+
+import pandas as pd
+import torch
+from PIL import Image
+import open_clip
 
 class Evaluate():
-    def __init__(self) -> None:
-        pass
+    """
+    Base class for evaluation
+    """
+    def __init__(self, **kwargs) -> None:
+        self.experiment_folder = os.path.join("data/results", kwargs.get("experiment_name", "default-experiment"))
+        self.hyperparameters = self.load_hyperparameters()
+
+
+    def load_hyperparameters(self):
+        """
+        Load hyperparameters from experiment folder
+        """
+        with open(os.path.join(self.experiment_folder, "hyperparameters.json"), "r") as f:
+            return json.load(f)
+        
+    def load_prompts(self, folder: str):
+        """
+        Load prompts from given prompt-specific folder containing generated images
+        """
+        with open(os.path.join(folder, "prompts.txt"), "r") as f:
+            prompts = f.read().splitlines()
+            prompts = [p for p in prompts if p != "" and p[0] != "<"]
+        return prompts
+    
+    def evaluate(self):
+        """
+        Evaluate generated image(s)
+        """
+        raise NotImplementedError
+    
+    def save_results(self):
+        """
+        Save evaluation results to file
+        """
+        raise NotImplementedError
+        
+class CLIPScore(Evaluate):
+    """
+    Evaluate generated images using CLIPScore-based metrics
+    """
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        
+        # Load CLIP model
+        self.model, _, self.preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
+        self.tokenizer = open_clip.get_tokenizer('ViT-B-32')
+
+        self.results_df = pd.DataFrame(columns=["prompt_id", "image_id", "score", "user_prompt", "optimized_prompt", "image_path"], index=["prompt_id", "image_id"])
+        
+    def evaluate(self):
+        """
+        Evaluate image using CLIP
+        """
+        # iterate over all prompts/generations of experiment
+        for prompt_folder in os.listdir(self.experiment_folder):
+            if not os.path.isdir(prompt_folder):
+                continue
+            prompt_folder = os.path.join(self.experiment_folder, prompt_folder)
+            prompts = self.load_prompts(prompt_folder)
+
+            # Tokenize and encode user prompt
+            user_prompt = self.tokenizer([prompts[0]])
+            user_prompt_features = self.model.encode_text(user_prompt)
+            user_prompt_features /= user_prompt_features.norm(dim=-1, keepdim=True)
+
+
+            # iterate over all generated images
+            images = []
+            images_features = []
+            for image_idx, prompt in enumerate(prompts):
+                # load, preprocess, and encode image
+                image = Image.open(os.path.join(prompt_folder, f"image_{image_idx}.png"))
+                images.append(image)
+                image = self.preprocess(image).unsqueeze(0)
+                image_features = self.model.encode_image(image)
+                image_features /= image_features.norm(dim=-1, keepdim=True)
+                images_features.append(image_features)
+
+                # calculate CLIP-based similarity of original prompt and generated image
+                #TODO check if this is best and correct way to implement scoring (consider vectorization)
+                score = (100.0 * image_features @ user_prompt_features).data.numpy()
+
+                df_row = {
+                    "prompt_id": int(prompt_folder.split("/")[-1]),
+                    "image_id": image_idx,
+                    "score": score,
+                    "user_prompt": prompts[0],
+                    "optimized_prompt": prompt,
+                    "image_path": os.path.join(prompt_folder, f"image_{image_idx}.png"),
+                }
+                self.results_df = self.results_df.append(df_row)
+
+
+        # Analyze results and obtain ultimate quantitative scores
+
+#TODO possible ways of obtaining scores:
+# - average score per image_id -> hard to make comparable across prompts
+# - number of occurances with score_optimized > score_user_prompt per image_id -> check if optimized prompt yields better results than user prompt
+# - number of occurances with score_optimized > score_user_prompt + all previous score_optimized -> check if continuing to optimize yields better results than previous optimization
+# - number of occurances with score_optimized > all other scores -> check how often best performance is obtained at given pipline iteration
