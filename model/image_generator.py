e77cbfe3880000e68dc9765c7eb1fc97468ed62c
Jona Ruthardt <jona@ruthardt.de>
2023-04-29 16:31:48 +0200

    Minor changes and bug fixes

--- model/image_generator.py
+++ model/image_generator.py
@@ -19,7 +19,7 @@ class ImageGeneratorType(enum.Enum):
 
 IMAGE_GENERATORS = frozenset(set([img_gen_t.value for img_gen_t in ImageGeneratorType]))
 
-def load_image_generator(model: str,  
+def load_image_generator(image_generator: str,  #TODO fix
                          device_map: Optional[Union[str, torch.device]]=None, 
                          torch_dtype:str|torch.dtype=torch.float16,
                          seed:int=None, 
@@ -33,6 +33,9 @@ def load_image_generator(model: str,
     Returns:
         ImageGenerator: instanciated and configured image generator sub-class
     """
+    print("Loading image generation model")
+
+    model = image_generator
     # Only the allowed types can be used
     if model not in IMAGE_GENERATORS:
         raise f"The Text-to-Image Generator given (\"{model}\") is not a recognised Generator from: {IMAGE_GENERATORS}."
