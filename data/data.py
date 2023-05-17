c81ed58887dca833f4f5ae5b6de33e4c0c991563
Jona Ruthardt <jona@ruthardt.de>
2023-05-17 18:59:32 +0200

    added medium parti dataset

--- data/data.py
+++ data/data.py
@@ -5,6 +5,7 @@ class DatasetType(enum.Enum):
     Flickr30k = "flickr30k"
     PartiPrompts = "parti-prompts"
     PartiPromptsSmall = "parti-prompts-small"
+    PartiPromptsMedium = "parti-prompts-medium"
     Flickr30kSmall = "flickr30k-small"
     CocoCaptionSmall = "cococaption-small"
     CocoCaptionMedium = "cococaption-medium"
